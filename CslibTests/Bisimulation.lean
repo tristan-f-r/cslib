d2f1bdb11d0372d14ecbcfda687972a0dcc8aee4
Fabrizio Montesi <famontesi@gmail.com>
2025-07-08 20:06:00 +0200

    missing license headers

--- CslibTests/Bisimulation.lean
+++ CslibTests/Bisimulation.lean
@@ -1,3 +1,9 @@
+/-
+Copyright (c) 2025 Fabrizio Montesi. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Fabrizio Montesi
+-/
+
 import Cslib.Semantics.LTS.Bisimulation
 
 /- An LTS with two bisimilar states. -/
