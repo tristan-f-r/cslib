f3fcfd96ec03f7d7c2544a7e567b2dcfc6a95a20
Chris Henson <chrishenson.net@gmail.com>
2025-07-14 15:51:41 -0400

    naming conventions

--- Cslib/Computability/LambdaCalculus/Untyped/LocallyNameless/Basic.lean
+++ Cslib/Computability/LambdaCalculus/Untyped/LocallyNameless/Basic.lean
@@ -24,46 +24,46 @@ variable {Var : Type u} [HasFresh Var] [DecidableEq Var]
 
 namespace LambdaCalculus.LocallyNameless
 
-/-- Syntax of locally nameless lambda terms, with free variables over `Var`. -/
+/-- Syntax of locally nameless absbda terms, with free variables over `Var`. -/
 inductive Term (Var : Type u)
-/-- Bound variables that appear under a lambda abstraction, using a de-Bruijn index. -/
+/-- Bound variables that appear under a absbda abstraction, using a de-Bruijn index. -/
 | bvar : ℕ → Term Var
 /-- Free variables. -/
 | fvar : Var → Term Var
 /-- Lambda abstraction, introducing a new bound variable. -/
-| lam  : Term Var → Term Var
+| abs  : Term Var → Term Var
 /-- Function application. -/
 | app  : Term Var → Term Var → Term Var
 
 /-- Variable opening of the ith bound variable. -/
 @[simp]
-def Term.open_rec (i : ℕ) (sub : Term Var) : Term Var → Term Var
+def Term.openRec (i : ℕ) (sub : Term Var) : Term Var → Term Var
 | bvar i' => if i = i' then sub else bvar i'
 | fvar x  => fvar x
-| app l r => app (open_rec i sub l) (open_rec i sub r)
-| lam M   => lam $ open_rec (i+1) sub M
+| app l r => app (openRec i sub l) (openRec i sub r)
+| abs M   => abs $ openRec (i+1) sub M
 
-scoped notation:68 e "⟦" i " ↝ " sub "⟧"=> Term.open_rec i sub e 
+scoped notation:68 e "⟦" i " ↝ " sub "⟧"=> Term.openRec i sub e 
 
 /-- Variable opening of the closest binding. -/
 @[simp]
-def Term.open' {X} (e u):= @Term.open_rec X 0 u e
+def Term.open' {X} (e u):= @Term.openRec X 0 u e
 
 infixr:80 " ^ " => Term.open'
 
 /-- Variable closing, replacing a free `fvar x` with `bvar k` -/
 @[simp]
-def Term.close_rec (k : ℕ) (x : Var) : Term Var → Term Var
+def Term.closeRec (k : ℕ) (x : Var) : Term Var → Term Var
 | fvar x' => if x = x' then bvar k else fvar x'
 | bvar i  => bvar i
-| app l r => app (close_rec k x l) (close_rec k x r)
-| lam t   => lam $ close_rec (k+1) x t
+| app l r => app (closeRec k x l) (closeRec k x r)
+| abs t   => abs $ closeRec (k+1) x t
 
-scoped notation:68 e "⟦" k " ↜ " x "⟧"=> Term.close_rec k x e 
+scoped notation:68 e "⟦" k " ↜ " x "⟧"=> Term.closeRec k x e 
 
 /-- Variable closing of the closest binding. -/
 @[simp]
-def Term.close {Var} [DecidableEq Var] (e u):= @Term.close_rec Var _ 0 u e
+def Term.close {Var} [DecidableEq Var] (e u):= @Term.closeRec Var _ 0 u e
 
 infixr:80 " ^* " => Term.close
 
@@ -74,7 +74,7 @@ def Term.subst (m : Term Var) (x : Var) (sub : Term Var) : Term Var :=
   | bvar i  => bvar i
   | fvar x' => if x = x' then sub else fvar x'
   | app l r => app (l.subst x sub) (r.subst x sub)
-  | lam M   => lam $ M.subst x sub
+  | abs M   => abs $ M.subst x sub
 
 scoped notation:67 e "[" x ":=" sub "]" => Term.subst e x sub
 
@@ -83,11 +83,11 @@ scoped notation:67 e "[" x ":=" sub "]" => Term.subst e x sub
 def Term.fv : Term Var → Finset Var
 | bvar _ => {}
 | fvar x => {x}
-| lam e1 => e1.fv
+| abs e1 => e1.fv
 | app l r => l.fv ∪ r.fv
 
 /-- Locally closed terms. -/
 inductive Term.LC : Term Var → Prop
 | fvar (x)  : LC (fvar x)
-| lam (L : Finset Var) (e : Term Var) : (∀ x : Var, x ∉ L → LC (e ^ fvar x)) → LC (lam e)
+| abs (L : Finset Var) (e : Term Var) : (∀ x : Var, x ∉ L → LC (e ^ fvar x)) → LC (abs e)
 | app {l r} : l.LC → r.LC → LC (app l r)
--- Cslib/Computability/LambdaCalculus/Untyped/LocallyNameless/Properties.lean
+++ Cslib/Computability/LambdaCalculus/Untyped/LocallyNameless/Properties.lean
@@ -19,20 +19,20 @@ lemma open_lc_aux (e : Term Var) : ∀ (j v i u),
   e = e ⟦i ↝ u⟧ := by
   induction' e 
   <;> intros j v i u neq h 
-  <;> simp only [open_rec, app.injEq, lam.injEq] at *
+  <;> simp only [openRec, app.injEq, abs.injEq] at *
   case bvar => aesop
   case app ih_l ih_r => 
     obtain ⟨hl, hr⟩ := h
     exact ⟨ih_l j v i u neq hl, ih_r j v i u neq hr⟩
-  case lam ih => exact ih (j+1) v (i+1) u (by aesop) h
+  case abs ih => exact ih (j+1) v (i+1) u (by aesop) h
 
 /-- Opening is associative for nonclashing free variables. -/
 lemma swap_open_fvars (k n : ℕ) (x y : Var) (m : Term Var) : 
     k ≠ n → x ≠ y → m⟦n ↝ fvar y⟧⟦k ↝ fvar x⟧ = m⟦k ↝ fvar x⟧⟦n ↝ fvar y⟧ := by
   revert k n
-  induction' m <;> intros k n ne_kn ne_xy <;> simp only [open_rec, app.injEq, lam.injEq]
+  induction' m <;> intros k n ne_kn ne_xy <;> simp only [openRec, app.injEq, abs.injEq]
   case bvar n' => aesop
-  case lam ih => apply ih <;> aesop
+  case abs ih => apply ih <;> aesop
   case app => aesop
 
 variable [DecidableEq Var]
@@ -45,9 +45,9 @@ theorem subst_fresh (x : Var) (t sub : Term Var) : x ∉ t.fv → (t [x := sub])
 lemma open_close (x : Var) (t : Term Var) (k : ℕ) : x ∉ t.fv → t⟦k ↝ fvar x⟧⟦k ↜ x⟧ = t := by
   intros mem
   revert k
-  induction t <;> intros k <;> simp only [open_rec, close_rec, app.injEq, lam.injEq]
+  induction t <;> intros k <;> simp only [openRec, closeRec, app.injEq, abs.injEq]
   case bvar n => split <;> simp_all
-  case lam t ih => exact ih mem (k + 1)
+  case abs t ih => exact ih mem (k + 1)
   case app l r ih_l ih_r => refine ⟨ih_l ?_ k, ih_r ?_ k⟩ <;> aesop
   all_goals aesop
 
@@ -55,7 +55,7 @@ lemma open_close (x : Var) (t : Term Var) (k : ℕ) : x ∉ t.fv → t⟦k ↝ f
 lemma open_injective (x : Var) (M M' : Term Var) : x ∉ M.fv → x ∉ M'.fv → M ^ fvar x = M' ^ fvar x → M = M' := by
   intros free_M free_M' eq
   rw [←open_close x M 0 free_M, ←open_close x M' 0 free_M']
-  exact congrArg (close_rec 0 x) eq
+  exact congrArg (closeRec 0 x) eq
 
 /-- Opening and closing are associative for nonclashing free variables. -/
 lemma swap_open_fvar_close (k n: ℕ) (x y : Var) (m : Term Var) : 
@@ -63,29 +63,29 @@ lemma swap_open_fvar_close (k n: ℕ) (x y : Var) (m : Term Var) :
   revert k n
   induction' m 
   <;> intros k n ne_kn ne_xy 
-  <;> simp only [open_rec, close_rec, app.injEq, lam.injEq]
+  <;> simp only [openRec, closeRec, app.injEq, abs.injEq]
   case bvar n'  => split <;> aesop
   case fvar x'  => split <;> aesop
-  case lam ih => apply ih <;> aesop
+  case abs ih => apply ih <;> aesop
   case app => aesop
 
 /-- Closing preserves free variables. -/
 lemma close_preserve_not_fvar {k x y} (m : Term Var) : x ∉ m.fv → x ∉ (m⟦k ↜ y⟧).fv := by
   intros mem
   revert k
-  induction m <;> intros k <;> simp only [close_rec]
+  induction m <;> intros k <;> simp only [closeRec]
   case fvar y' => split <;> aesop
-  case lam ih => exact ih mem
+  case abs ih => exact ih mem
   all_goals aesop
 
 /-- Opening to a fresh free variable preserves free variables. -/
 lemma open_fresh_preserve_not_fvar {k x y} (m : Term Var) : x ∉ m.fv → x ≠ y → x ∉ (m⟦k ↝ fvar y⟧).fv := by
   intros mem neq
   revert k
-  induction m <;> intros k <;> simp only [open_rec]
+  induction m <;> intros k <;> simp only [openRec]
   case bvar n'  => split <;> aesop
   case fvar => aesop
-  case lam ih => exact ih mem
+  case abs ih => exact ih mem
   all_goals aesop
 
 /-- Substitution preserves free variables. -/
@@ -94,15 +94,15 @@ lemma subst_preserve_not_fvar {x y} (m n : Term Var) : x ∉ m.fv ∪ n.fv → x
   simp only [Finset.mem_union, not_or] at mem
   induction m <;> simp only [subst]
   case fvar y' => split <;> simp only [fv, Finset.mem_singleton, mem] <;> aesop
-  case lam ih => exact ih mem
+  case abs ih => exact ih mem
   all_goals aesop
 
 /-- Closing removes a free variable. -/
 lemma close_var_not_fvar_rec (x) (k) (t : Term Var) : x ∉ (t⟦k ↜ x⟧).fv := by
   revert k
-  induction t <;> intros k <;> simp only [close_rec]
+  induction t <;> intros k <;> simp only [closeRec]
   case fvar x' => split <;> simp_all
-  case lam ih => exact ih (k + 1)
+  case abs ih => exact ih (k + 1)
   all_goals aesop
 
 /-- Specializes `close_var_not_fvar_rec` to first closing. -/
@@ -115,21 +115,21 @@ omit [DecidableEq Var] in
 lemma open_lc (k t) (e : Term Var) : e.LC → e = e⟦k ↝ t⟧ := by
   intros e_lc
   revert k
-  induction e_lc <;> intros k <;> simp only [open_rec, app.injEq, lam.injEq]
+  induction e_lc <;> intros k <;> simp only [openRec, app.injEq, abs.injEq]
   case app => aesop
-  case lam xs e _ ih => refine open_lc_aux e 0 (fvar (fresh xs)) (k+1) t ?_ ?_ <;> aesop
+  case abs xs e _ ih => refine open_lc_aux e 0 (fvar (fresh xs)) (k+1) t ?_ ?_ <;> aesop
 
 /-- Substitution of a locally closed term distributes with opening. -/
 lemma subst_open (x : Var) (t : Term Var) (k : ℕ) (u e) :
   LC t → 
   (e ⟦ k ↝ u ⟧) [ x := t ] = (e [ x := t ]) ⟦k ↝  u [ x := t ]⟧ := by
   revert k
-  induction' e <;> intros k t_lv <;> simp only [open_rec, subst, app.injEq, lam.injEq]
+  induction' e <;> intros k t_lv <;> simp only [openRec, subst, app.injEq, abs.injEq]
   case bvar k' => aesop
   case fvar x' => 
     split <;> simp_all
     exact open_lc k (u[x':=t]) t t_lv
-  case lam ih => exact ih (k + 1) t_lv
+  case abs ih => exact ih (k + 1) t_lv
   case app ih_l ih_r => exact ⟨ih_l k t_lv, ih_r k t_lv⟩
 
 /-- Specialize `subst_open` to the first opening. -/
@@ -144,8 +144,8 @@ theorem subst_lc {x : Var} {e u : Term Var} : LC e → LC u → LC (e [x := u]) 
   induction lc_e <;> simp only [subst]
   case fvar => split <;> [assumption; constructor] 
   case app ih_l ih_r => exact LC.app ih_l ih_r
-  case lam xs e _ ih =>
-    refine LC.lam ({x} ∪ xs) _ ?_
+  case abs xs e _ ih =>
+    refine LC.abs ({x} ∪ xs) _ ?_
     intros y mem
     rw [subst_open_var y x u e ?_ lc_u]
     apply ih
@@ -161,10 +161,10 @@ lemma subst_intro (x : Var) (t e : Term Var) : x ∉ e.fv → LC t → e ^ t = (
   exact mem
 
 /-- Opening of locally closed terms is locally closed. -/
-theorem beta_lc {M N : Term Var} : LC (lam M) → LC N → LC (M ^ N) := by
+theorem beta_lc {M N : Term Var} : LC (abs M) → LC N → LC (M ^ N) := by
   intros m_lc
   cases m_lc
-  case lam xs mem =>
+  case abs xs mem =>
     intros n_lc
     have ⟨y, ymem⟩ := fresh_exists (xs ∪ M.fv)
     simp only [Finset.mem_union, not_or] at ymem
@@ -178,10 +178,10 @@ theorem beta_lc {M N : Term Var} : LC (lam M) → LC N → LC (M ^ N) := by
 lemma open_close_to_subst (m : Term Var) (x y : Var) (k : ℕ) : LC m → m ⟦k ↜ x⟧⟦k ↝ fvar y⟧ = m [x := fvar y] := by
   intros m_lc
   revert k
-  induction' m_lc <;> intros k <;> simp only [close_rec, open_rec, subst, lam.injEq, app.injEq]
+  induction' m_lc <;> intros k <;> simp only [closeRec, openRec, subst, abs.injEq, app.injEq]
   case fvar x' => split <;> simp
   case app ih_l ih_r => exact ⟨ih_l _, ih_r _⟩
-  case lam xs t x_mem ih =>
+  case abs xs t x_mem ih =>
     have ⟨x', x'_mem⟩ := fresh_exists ({x} ∪ {y} ∪ t.fv ∪ xs)
     have s := subst_open_var x' x (fvar y) t ?_ (by constructor)
     simp only [open', Finset.union_assoc, Finset.mem_union, Finset.mem_singleton, not_or] at *
@@ -200,9 +200,9 @@ lemma open_close_to_subst (m : Term Var) (x y : Var) (k : ℕ) : LC m → m ⟦k
 lemma close_open (x : Var) (t : Term Var) (k : ℕ) : LC t → t⟦k ↜ x⟧⟦k ↝ fvar x⟧ = t := by
   intros lc_t
   revert k
-  induction lc_t <;> intros k <;> simp only [close_rec, open_rec, lam.injEq, app.injEq]
+  induction lc_t <;> intros k <;> simp only [closeRec, openRec, abs.injEq, app.injEq]
   case fvar x' => split <;> simp_all
-  case lam xs t t_open_lc ih => 
+  case abs xs t t_open_lc ih => 
     have ⟨y, hy⟩ := fresh_exists (xs ∪ t.fv ∪ (t⟦k + 1 ↜ x⟧⟦k + 1 ↝ fvar x⟧).fv ∪ {x})
     simp only [Finset.union_assoc, Finset.mem_union, Finset.mem_singleton, not_or] at hy
     obtain ⟨q1, q2, q3, q4⟩ := hy
