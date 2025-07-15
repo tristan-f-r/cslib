095458a4e5bde9b615a139d41a38aedc7281334b
Chris Henson <chrishenson.net@gmail.com>
2025-07-15 16:16:03 -0400

    Reduction Notation

---
+++ Cslib/Syntax/ReductionNotation.lean
@@ -0,0 +1,43 @@
+import Mathlib
+
+open Lean Lean.Elab Lean.Meta
+
+/-- 
+  This command adds notations for a relation, its reflexive transitive closure, and its
+  equivalence closure. This should not usually be called directly, but as an attribute. 
+
+  As an example `reduce_notation foo "β"` will add the notations "⇢β", "↠β", and "≈β".
+
+  Note that the string used will afterwards be registered as a notation. This
+  means that if you have also used this as a constructor name, you will be
+  quotes to access corresponding cases, e.g. «β» in the above example.
+-/
+syntax "reduce_notation" ident Lean.Parser.Command.notationItem : command
+macro_rules
+  | `(reduce_notation $rel $sym) => 
+    `(
+      notation:39 t " ⇢"$sym t' => $rel t t'
+      notation:39 t " ↠"$sym t' => Relation.ReflTransGen $rel t t'
+      notation:39 t " ≈"$sym t' => Relation.EqvGen $rel t t'
+     )
+
+/-- 
+  This attribute creates calls the `reduce_notation` command for the annotated
+  declaration, such as in:
+
+  ```
+  @[reduce_notation "ₙ"]
+  def PredReduction (a b : ℕ) : Prop := a = b + 1
+  ```
+-/
+syntax (name := reduce_notation_attr) "reduce_notation" Lean.Parser.Command.notationItem : attr
+
+initialize Lean.registerBuiltinAttribute {
+  name := `reduce_notation_attr
+  descr := ""
+  add := fun decl stx kind => MetaM.run' do
+    let `(attr | reduce_notation $sym) := stx 
+     | throwError "invalid syntax for 'reduce_notation' attribute"
+    let cmd ← `(reduce_notation $(mkIdent decl) $sym)
+    liftCommandElabM <| Command.elabCommand cmd
+}
---
+++ CslibTests/ReductionNotation.lean
@@ -0,0 +1,18 @@
+import Cslib.Syntax.ReductionNotation
+
+@[reduce_notation "ₙ", simp]
+def PredReduction (a b : ℕ) : Prop := a = b + 1
+
+open Relation.ReflTransGen
+
+lemma single_step : 5 ⇢ₙ 4 := by simp
+
+lemma multiple_step : 5 ↠ₙ 3 := by
+  have s1 : 5 ↠ₙ 4 := by
+    apply single
+    simp
+  refine Relation.ReflTransGen.trans s1 ?_
+  apply single
+  simp
+ 
+lemma equiv_step : 5 ≈ₙ 5 := Relation.EqvGen.refl 5
