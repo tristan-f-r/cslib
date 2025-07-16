c6f6087f8fb14967e27e4f21c117729d8600865d
Chris Henson <chrishenson.net@gmail.com>
2025-07-16 18:54:11 -0400

    style

--- Cslib/Semantics/ReductionSystem/Basic.lean
+++ Cslib/Semantics/ReductionSystem/Basic.lean
@@ -40,17 +40,17 @@ theorem ReductionSystem.MRed.single (rs : ReductionSystem Term) (h : rs.Red a b)
   rs.MRed a b :=
   Relation.ReflTransGen.single h
 
-open Relation.ReflTransGen
+open Relation Relation.ReflTransGen
 
 -- these instance allow us to switch between single and multistep reductions in a `calc` block
-instance {α} (R : α → α → Prop) : Trans R R (Relation.ReflTransGen R) where
+instance {α} (R : α → α → Prop) : Trans R R (ReflTransGen R) where
   trans hab hbc := head hab (single hbc)
 
-instance {α} (R : α → α → Prop) : Trans R (Relation.ReflTransGen R) (Relation.ReflTransGen R) where
-  trans hab hbc := head hab hbc
+instance {α} (R : α → α → Prop) : Trans R (ReflTransGen R) (ReflTransGen R) where
+  trans := head
 
-instance {α} (R : α → α → Prop) : Trans (Relation.ReflTransGen R) R (Relation.ReflTransGen R) where
-  trans hab hbc := tail hab hbc
+instance {α} (R : α → α → Prop) : Trans (ReflTransGen R) R (ReflTransGen R) where
+  trans := tail
 
 instance (rs : ReductionSystem Term) : Trans rs.Red rs.Red rs.MRed  := by infer_instance
 instance (rs : ReductionSystem Term) : Trans rs.Red rs.MRed rs.MRed := by infer_instance
