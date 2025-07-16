82a63b7ee980fdfb0fea54c0d7056087fce32c15
Fabrizio Montesi <famontesi@gmail.com>
2025-07-16 16:11:31 +0200

    Reduction systems

---
+++ Cslib/Semantics/ReductionSystem/Basic.lean
@@ -0,0 +1,43 @@
+/-
+Copyright (c) 2025 Fabrizio Montesi. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Fabrizio Montesi, Thomas Waring
+-/
+
+import Mathlib.Logic.Relation
+
+/-!
+# Reduction System
+
+A reduction system is an operational semantics expressed as a relation between terms.
+-/
+
+universe u
+
+/--
+A reduction system is a relation between `Term`s.
+-/
+structure ReductionSystem (Term : Type u) where
+  /-- The reduction relation. -/
+  Red : Term → Term → Prop
+
+
+section MultiStep
+
+/-! ## Multi-step reductions -/
+
+/-- Multi-step reduction relation. -/
+def ReductionSystem.MRed (rs : ReductionSystem Term) :=
+  Relation.ReflTransGen rs.Red
+
+/-- All multi-step reduction relations are reflexive. -/
+@[refl]
+theorem ReductionSystem.MRed.refl (rs : ReductionSystem Term) (t : Term) : rs.MRed t t :=
+  Relation.ReflTransGen.refl
+
+/-- Any reduction is a multi-step -/
+theorem ReductionSystem.MRed.single (rs : ReductionSystem Term) (h : rs.Red a b) :
+  rs.MRed a b :=
+  Relation.ReflTransGen.single h
+
+end MultiStep
