06788112386703e09f0a467f6bf297e554d0dbaf
Tristan F.-R. <pub.tristanf@gmail.com>
2025-08-16 03:34:12 -0700

    feat(LinearLogic): logical equivalence is an equivalence relation (#34)
    * feat(LinearLogic): logical equivalence is an equivalence relation
    
    * use ʔ

--- Cslib/Logic/LinearLogic/CLL/Basic.lean
+++ Cslib/Logic/LinearLogic/CLL/Basic.lean
@@ -11,7 +11,6 @@ import Mathlib.Order.Notation
 
 ## TODO
 - First-order polymorphism.
-- Logical equivalences.
 - Cut elimination.
 
 ## References
@@ -22,46 +21,49 @@ import Mathlib.Order.Notation
 
 universe u
 
-section CLL
-
 variable {Atom : Type u}
 
 namespace CLL
 
 /-- Propositions. -/
-inductive Proposition {Atom : Type u} : Type u where
+inductive Proposition (Atom : Type u) : Type u where
   | atom (x : Atom)
   | atomDual (x : Atom)
   | one
   | zero
   | top
   | bot
-  | tensor (a b : @Proposition Atom)
-  | parr (a b : @Proposition Atom)
-  | oplus (a b : @Proposition Atom)
-  | with (a b : @Proposition Atom)
-  | bang (a : @Proposition Atom)
-  | quest (a : @Proposition Atom)
+  /-- The multiplicative conjunction connective. -/
+  | tensor (a b : Proposition Atom)
+  /-- The multiplicative disjunction connective. -/
+  | parr (a b : Proposition Atom)
+  /-- The additive disjunction connective. -/
+  | oplus (a b : Proposition Atom)
+  /-- The additive conjunction connective. -/
+  | with (a b : Proposition Atom)
+  /-- The "of course" exponential. -/
+  | bang (a : Proposition Atom)
+  /-- The "why not" exponential.
+  This is written as ʔ, or \_?, to distinguish itself from the lean syntatical hole ? syntax -/
+  | quest (a : Proposition Atom)
 deriving DecidableEq, BEq
 
-instance : One (@Proposition Atom) where
-  one := Proposition.one
+instance : Zero (Proposition Atom) := ⟨.zero⟩
+instance : One (Proposition Atom) := ⟨.one⟩
 
-instance : Zero (@Proposition Atom) where
-  zero := Proposition.zero
+instance : Top (Proposition Atom) := ⟨.top⟩
+instance : Bot (Proposition Atom) := ⟨.bot⟩
 
-instance : Top (@Proposition Atom) where
-  top := Proposition.top
+@[inherit_doc] scoped infix:35 " ⊗ " => Proposition.tensor
+@[inherit_doc] scoped infix:35 " ⊕ " => Proposition.oplus
+@[inherit_doc] scoped infix:30 " ⅋ " => Proposition.parr
+@[inherit_doc] scoped infix:30 " & " => Proposition.with
 
-instance : Bot (@Proposition Atom) where
-  bot := Proposition.bot
-
-scoped infix:35 " ⊗ " => Proposition.tensor
-scoped infix:30 " ⅋ " => Proposition.parr
+@[inherit_doc] scoped prefix:95 "!" => Proposition.bang
+@[inherit_doc] scoped prefix:95 "ʔ" => Proposition.quest
 
 /-- Positive propositions. -/
-def Proposition.Pos (a : @Proposition Atom) : Prop :=
-  match a with
+def Proposition.Pos : Proposition Atom → Prop
   | atom _ => True
   | one => True
   | zero => True
@@ -71,31 +73,27 @@ def Proposition.Pos (a : @Proposition Atom) : Prop :=
   | _ => False
 
 /-- Negative propositions. -/
-def Proposition.Neg (a : @Proposition Atom) : Prop :=
-  match a with
+def Proposition.Neg : Proposition Atom → Prop
   | atomDual _ => True
   | bot => True
   | top => True
   | parr _ _ => True
-  | Proposition.with _ _ => True
+  | .with _ _ => True
   | quest _ => True
   | _ => False
 
 /-- Whether a `Proposition` is positive is decidable. -/
-instance Proposition.pos_decidable (a : @Proposition Atom) : Decidable a.Pos := by
-  cases a <;> 
-  simp only [Proposition.Pos] <;> 
-  first | apply Decidable.isTrue; trivial | apply Decidable.isFalse; trivial
+instance Proposition.pos_decidable (a : Proposition Atom) : Decidable a.Pos := by
+  unfold Proposition.Pos
+  split <;> infer_instance
 
 /-- Whether a `Proposition` is negative is decidable. -/
-instance Proposition.neg_decidable (a : @Proposition Atom) : Decidable a.Neg := by
-  cases a <;> 
-  simp only [Proposition.Neg] <;> 
-  first | apply Decidable.isTrue; trivial | apply Decidable.isFalse; trivial
+instance Proposition.neg_decidable (a : Proposition Atom) : Decidable a.Neg := by
+  unfold Proposition.Neg
+  split <;> infer_instance
 
 /-- Propositional duality. -/
-def Proposition.dual (a : @Proposition Atom) : @Proposition Atom :=
-  match a with
+def Proposition.dual : Proposition Atom → Proposition Atom
   | atom x => atomDual x
   | atomDual x => atom x
   | one => bot
@@ -104,68 +102,55 @@ def Proposition.dual (a : @Proposition Atom) : @Proposition Atom :=
   | top => zero
   | tensor a b => parr a.dual b.dual
   | parr a b => tensor a.dual b.dual
-  | oplus a b => Proposition.with a.dual b.dual
-  | Proposition.with a b => oplus a.dual b.dual
+  | oplus a b => .with a.dual b.dual
+  | .with a b => oplus a.dual b.dual
   | bang a => quest a.dual
   | quest a => bang a.dual
 
 /-- No proposition is equal to its dual. -/
-theorem Proposition.dual.neq (a : @Proposition Atom) : a ≠ a.dual := by
+theorem Proposition.dual.neq (a : Proposition Atom) : a ≠ a.dual := by
   cases a <;> simp [Proposition.dual]
 
 /-- Two propositions are equal iff their respective duals are equal. -/
-theorem Proposition.dual.eq_iff (a b : @Proposition Atom) : a = b ↔ a.dual = b.dual := by
-  apply Iff.intro <;> intro h
-  · cases a <;> cases b <;> simp at h <;> simp [h]
-  · induction a generalizing b <;> cases b
-    all_goals try cases h
-    all_goals try rfl
-    all_goals simp_all [Proposition.dual]; aesop
+@[simp]
+theorem Proposition.dual_inj (a b : Proposition Atom) : a.dual = b.dual ↔ a = b := by
+  refine ⟨fun h ↦ ?_, congrArg dual⟩
+  induction a generalizing b <;> cases b
+  all_goals aesop (add simp [Proposition.dual])
 
 /-- Duality is an involution. -/
-theorem Proposition.dual.involution (a : @Proposition Atom) : a.dual.dual = a := by
-  induction a <;> simp only [dual]
-  case tensor a b iha ihb =>
-    simp only [iha, ihb]
-  case parr a b iha ihb =>
-    simp only [iha, ihb]
-  case oplus a b iha ihb =>
-    simp only [iha, ihb]
-  case _ a b iha ihb =>
-    simp only [iha, ihb]
-  case bang a iha =>
-    simp only [iha]
-  case quest a iha =>
-    simp only [iha]
+@[simp]
+theorem Proposition.dual.involution (a : Proposition Atom) : a.dual.dual = a := by
+  induction a <;> simp_all [dual]
 
 /-- Linear implication. -/
-def Proposition.linImpl (a b : @Proposition Atom) : @Proposition Atom := a.dual.parr b
+def Proposition.linImpl (a b : Proposition Atom) : Proposition Atom := a.dual ⅋ b
 
 /-- A sequent in CLL is a list of propositions. -/
-abbrev Sequent := List (@Proposition Atom)
+abbrev Sequent (Atom) := List (Proposition Atom)
 
 /-- Checks that all propositions in `Γ` are question marks. -/
-def Sequent.allQuest (Γ : @Sequent Atom) :=
+def Sequent.allQuest (Γ : Sequent Atom) :=
   ∀ a ∈ Γ, ∃ b, a = Proposition.quest b
 
 open Proposition in
 /-- Sequent calculus for CLL. -/
-inductive Proof : @Sequent Atom → Prop where
+inductive Proof : Sequent Atom → Prop where
   | ax : Proof [a, a.dual]
   | cut : Proof (a :: Γ) → Proof (a.dual :: Δ) → Proof (Γ ++ Δ)
   | exchange : List.Perm Γ Δ → Proof Γ → Proof Δ
   | one : Proof [one]
-  | bot : Proof Γ → Proof (bot :: Γ)
-  | parr : Proof (a :: b :: Γ) → Proof ((parr a b) :: Γ)
-  | tensor : Proof (a :: Γ) → Proof (b :: Δ) → Proof ((tensor a b) :: (Γ ++ Δ))
-  | oplus₁ : Proof (a :: Γ) → Proof ((oplus a b) :: Γ)
-  | oplus₂ : Proof (b :: Γ) → Proof ((oplus a b) :: Γ)
-  | with : Proof (a :: Γ) → Proof (b :: Γ) → Proof ((a.with b) :: Γ)
+  | bot : Proof Γ → Proof (⊥ :: Γ)
+  | parr : Proof (a :: b :: Γ) → Proof ((a ⅋ b) :: Γ)
+  | tensor : Proof (a :: Γ) → Proof (b :: Δ) → Proof ((a ⊗ b) :: (Γ ++ Δ))
+  | oplus₁ : Proof (a :: Γ) → Proof ((a ⊕ b) :: Γ)
+  | oplus₂ : Proof (b :: Γ) → Proof ((a ⊕ b) :: Γ)
+  | with : Proof (a :: Γ) → Proof (b :: Γ) → Proof ((a & b) :: Γ)
   | top : Proof (top :: Γ)
-  | quest : Proof (a :: Γ) → Proof (quest a :: Γ)
-  | weaken : Proof Γ → Proof (quest a :: Γ)
-  | contract : Proof (quest a :: quest a :: Γ) → Proof (quest a :: Γ)
-  | bang {Γ : @Sequent Atom} {a} : Γ.allQuest → Proof (a :: Γ) → Proof (bang a :: Γ)
+  | quest : Proof (a :: Γ) → Proof (ʔa :: Γ)
+  | weaken : Proof Γ → Proof (ʔa :: Γ)
+  | contract : Proof (ʔa :: ʔa :: Γ) → Proof (ʔa :: Γ)
+  | bang {Γ : Sequent Atom} {a} : Γ.allQuest → Proof (a :: Γ) → Proof ((!a) :: Γ)
 
 scoped notation "⊢" Γ:90 => Proof Γ
 
@@ -174,14 +159,33 @@ section LogicalEquiv
 /-! ## Logical equivalences -/
 
 /-- Two propositions are equivalent if one implies the other and vice versa. -/
-def Proposition.equiv (a b : @Proposition Atom) : Prop := ⊢[a.dual, b] ∧ ⊢[b.dual, a]
+def Proposition.equiv (a b : Proposition Atom) : Prop := ⊢[a.dual, b] ∧ ⊢[b.dual, a]
 
 scoped infix:29 " ≡ " => Proposition.equiv
 
 namespace Proposition
 
-/-- !⊤ ≡ 1 -/
-theorem bang_top_eqv_one : (@bang Atom ⊤) ≡ 1 := by
+@[refl]
+theorem refl (a : Proposition Atom) : a ≡ a := by
+  constructor
+  all_goals
+    apply Proof.exchange (List.Perm.swap ..)
+    exact Proof.ax
+
+@[symm]
+theorem symm {a b : Proposition Atom} (h : a ≡ b) : b ≡ a := ⟨h.2, h.1⟩
+
+theorem trans {a b c : Proposition Atom} (hab : a ≡ b) (hbc : b ≡ c) : a ≡ c :=
+  ⟨
+    Proof.cut (Proof.exchange (List.Perm.swap ..) hab.1) hbc.1,
+    Proof.cut (Proof.exchange (List.Perm.swap ..) hbc.2) hab.2
+  ⟩
+
+/-- The canonical equivalence relation for propositions. -/
+def propositionSetoid : Setoid (Proposition Atom) :=
+  ⟨equiv, refl, symm, trans⟩
+
+theorem bang_top_eqv_one : (!⊤ : Proposition Atom) ≡ 1 := by
   constructor
   · apply Proof.weaken
     exact Proof.one
@@ -190,8 +194,7 @@ theorem bang_top_eqv_one : (@bang Atom ⊤) ≡ 1 := by
     · intro _ _; contradiction
     exact Proof.top
 
-/-- ?0 ≡ ⊥ -/
-theorem quest_zero_eqv_bot : (@quest Atom 0) ≡ ⊥ := by
+theorem quest_zero_eqv_bot : (ʔ0 : Proposition Atom) ≡ ⊥ := by
   constructor
   · apply Proof.exchange (List.Perm.swap (bang top) bot [])
     apply Proof.bot
@@ -202,17 +205,13 @@ theorem quest_zero_eqv_bot : (@quest Atom 0) ≡ ⊥ := by
     apply Proof.weaken
     exact Proof.one
 
-/-- a ⊗ 0 ≡ 0 -/
-theorem tensor_zero_eqv_zero (a : @Proposition Atom) :
-    a ⊗ 0 ≡ 0 := by
-  constructor
-  · apply Proof.parr
-    apply Proof.exchange (List.Perm.swap a.dual (top) [zero])
-    exact Proof.top
-  · exact Proof.top
+theorem tensor_zero_eqv_zero (a : Proposition Atom) : a ⊗ 0 ≡ 0 := by
+  refine ⟨?_, .top⟩
+  apply Proof.parr
+  apply Proof.exchange (List.Perm.swap a.dual ⊤ [0])
+  exact Proof.top
 
-/-- a ⅋ ⊤ ≡ ⊤ -/
-theorem parr_top_eqv_top (a : @Proposition Atom) :
+theorem parr_top_eqv_top (a : Proposition Atom) :
     a ⅋ ⊤ ≡ ⊤ := by
   constructor
   · apply Proof.exchange (List.Perm.swap (parr a top).dual top [])
@@ -225,7 +224,5 @@ theorem parr_top_eqv_top (a : @Proposition Atom) :
 end Proposition
 
 end LogicalEquiv
-
-end CLL
 
 end CLL
